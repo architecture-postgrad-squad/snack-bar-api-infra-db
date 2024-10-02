39358224cf098223164b91311417727cafdcadc1
Eduardo Petri <54587346+eduardopetri@users.noreply.github.com>
2024-10-01 21:45:34 -0300

    Revert "[chore] Add infra db"

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_instance_endpoint" {
-  description = "The endpoint of the RDS instance"
-  value       = aws_db_instance.postgres.endpoint
-}
-
-output "db_instance_identifier" {
-  description = "The RDS instance identifier"
-  value       = aws_db_instance.postgres.id
-}
